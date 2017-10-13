ec07a76720fd358b7de27649d6c27324d2787dd0
dima.dosta <dima.dosta9@gmail.com>
2017-10-13 11:04:31 +0300

    as

--- deploy.bat
+++ deploy.bat
@@ -1,4 +1,3 @@
 @echo off
 for /f "delims=" %%x in (connection_string.txt) do set Build=%%x
-rh --dt postgres --silent -c="%Build%" -r=https://github.com/itdoster/bd_transfer --sqlfilesdirectory=scripts
-pause
+rh --dt postgres --silent -c="%Build%" -r=https://github.com/itdoster/postgres_continues_integration.git --sqlfilesdirectory=scripts
