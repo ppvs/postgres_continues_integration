5d87b08c74b1773f6ff8a6fceaa3cd203d137796
dima.dosta <dima.dosta9@gmail.com>
2017-10-13 07:11:42 +0300

    asd

--- deploy.bat
+++ deploy.bat
@@ -1,3 +1,6 @@
 @echo off
 for /f "delims=" %%x in (connection_string.txt) do set Build=%%x
 rh --dt=postgres --silent -c="%Build%" -t=true -r=https://github.com/itdoster/bd_transfer --sqlfilesdirectory=scripts
+if "%errorlevel%"=="0" cls &Echo Success.
+if "%errorlevel%"=="1" cls &Echo Fail
+endlocal
