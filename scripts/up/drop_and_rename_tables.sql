1e8a89490c8c14258c6ea16cc25b02e3c48030db
dima.dosta <dima.dosta9@gmail.com>
2017-10-13 14:09:00 +0300

    change script

--- scripts/up/drop_and_rename_tables.sql
+++ scripts/up/drop_and_rename_tables.sql
@@ -26,25 +26,29 @@ ALTER TABLE tuser_myuser RENAME TO myuser;
 
 
 CREATE TABLE IF NOT EXISTS material (
-	id	serial primary key,
-	name	text
+	id		serial primary key,
+	name		text
 );
-CREATE TABLE IF NOT EXISTS handle (
-	id	serial primary key,
-	bim_handle_type	text
+
+CREATE TABLE IF NOT EXISTS bim_handle (
+	id		serial primary key,
+	bim_handle_type	text,
+	bimdocument_id 	integer references bimdocument(id)
 );
+
 CREATE TABLE IF NOT EXISTS description (
-	id	serial primary key,
+	id		serial primary key,
 	source_type	text,
 	source_id	text,
 	category	text,
-	family	text,
+	family		text,
 	element_type	text,
-	bim_handle_id	integer
+	bim_handle_id	integer references bim_handle(id)
 );
+
 CREATE TABLE IF NOT EXISTS raw_data (
-	id	serial primary key,
-	key	text,
+	id		serial primary key,
+	key		text,
 	storage_type	text,
 	double_value	double precision,
 	string_value	text,
@@ -52,37 +56,38 @@ CREATE TABLE IF NOT EXISTS raw_data (
 );
 
 CREATE TABLE IF NOT EXISTS thermal_data_map (
+	id		serial primary key,
 	material_id	integer references material(id),
 	raw_data_id	integer references raw_data(id)
 );
+
 CREATE TABLE IF NOT EXISTS structural_data_map (
+	id		serial primary key,
 	material_id	integer references material(id),
 	raw_data_id	integer references raw_data(id)
 );
 
 CREATE TABLE IF NOT EXISTS material_map (
-	bim_handle_id	integer references handle(id),
+	id		serial primary key,
+	bim_handle_id	integer references bim_handle(id),
 	material_id	integer references material(id)
 );
 
 CREATE TABLE IF NOT EXISTS geometry_item (
-	id	serial primary key,
+	id		serial primary key,
 	geometry_type	text,
-	data	text,
-	bim_handle_id	integer references handle(id)
+	data		text,
+	bim_handle_id	integer references bim_handle(id)
 );
 CREATE TABLE IF NOT EXISTS description_data_map (
+	id		serial primary key,
 	description_id	integer references description(id),
 	raw_data_id	integer references raw_data(id)
 );
 
 CREATE TABLE IF NOT EXISTS bim_rel (
-	main_id	integer,
-	related_id	integer,
-	bim_rel_type	text,
-    bimdocuments_id integer references bimdocument(id)
+	id		serial primary key,
+	main_id		integer references bim_handle(id),
+	related_id	integer references bim_handle(id),
+	bim_rel_type	text
 );
-CREATE TABLE IF NOT EXISTS bim_meta (
-	version	text
-);
-
